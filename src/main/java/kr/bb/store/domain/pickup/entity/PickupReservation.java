7c2d2758220bc33b2218b54571e4874554c5c888
qwerty1434 <qwerty1434@naver.com>
2023-11-21 19:36:18 +0900

    :sparkles: PickupReservation 엔티티 생성 #LF1-212

---
+++ src/main/java/kr/bb/store/domain/pickup/entity/PickupReservation.java
@@ -0,0 +1,37 @@
+package kr.bb.store.domain.pickup.entity;
+
+import kr.bb.store.domain.store.entity.Store;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class PickupReservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="store_id", nullable = false)
+    private Store store;
+    @NotNull
+    private Long userId;
+    @NotNull
+    private Long orderPickupId;
+    @NotNull
+    private Long productId;
+    @NotNull
+    private String reservationCode;
+    @NotNull
+    private LocalDateTime pickupDate;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus reservationStatus;
+
+}
