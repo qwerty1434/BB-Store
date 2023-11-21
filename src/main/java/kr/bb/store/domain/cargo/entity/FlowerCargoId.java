8e9bfccaa36c2cce4dc9efabe71592e15064e2a4
qwerty1434 <qwerty1434@naver.com>
2023-11-21 20:58:19 +0900

    :recycle: 복합키 Id에 equals와 hashCode override

--- src/main/java/kr/bb/store/domain/cargo/entity/FlowerCargoId.java
+++ src/main/java/kr/bb/store/domain/cargo/entity/FlowerCargoId.java
@@ -2,9 +2,28 @@ package kr.bb.store.domain.cargo.entity;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class FlowerCargoId implements Serializable {
     private Long storeId;
     private Long flowerId;
+
+
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FlowerCargoId that = (FlowerCargoId) o;
+        return Objects.equals(storeId, that.storeId) && Objects.equals(flowerId, that.flowerId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(storeId, flowerId);
+    }
 }
--- src/main/java/kr/bb/store/domain/coupon/entity/IssuedCouponId.java
+++ src/main/java/kr/bb/store/domain/coupon/entity/IssuedCouponId.java
@@ -2,9 +2,28 @@ package kr.bb.store.domain.coupon.entity;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class IssuedCouponId implements Serializable {
     private Long couponId;
     private Long userId;
+
+
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IssuedCouponId that = (IssuedCouponId) o;
+        return Objects.equals(couponId, that.couponId) && Objects.equals(userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(couponId, userId);
+    }
 }
