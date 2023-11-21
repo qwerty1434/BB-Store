196876615c6450c25b9a3ca33a15f8000f7cd574
qwerty1434 <qwerty1434@naver.com>
2023-11-21 18:00:28 +0900

    :sparkles: FlowerCargo 엔티티 생성 #LF1-197

---
+++ src/main/java/kr/bb/store/domain/cargo/entity/FlowerCargo.java
@@ -0,0 +1,23 @@
+package kr.bb.store.domain.cargo.entity;
+
+import kr.bb.store.domain.store.entity.Store;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class FlowerCargo {
+    @EmbeddedId
+    private FlowerCargoId id;
+
+    @MapsId("storeId")
+    @ManyToOne
+    @JoinColumn(name="store_id")
+    private Store store;
+
+    private Long stock;
+}
---
+++ src/main/java/kr/bb/store/domain/cargo/entity/FlowerCargoId.java
@@ -0,0 +1,10 @@
+package kr.bb.store.domain.cargo.entity;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class FlowerCargoId implements Serializable {
+    private Long storeId;
+    private Long flowerId;
+}
