899cb36a28adb759927c0fd7ddbf7fb99cf15de4
qwerty1434 <qwerty1434@naver.com>
2023-11-21 15:02:26 +0900

    :tada: ControllerAdvice 생성 #LF1-238

---
+++ src/main/java/kr/bb/store/exception/advice/ControllerAdvice.java
@@ -0,0 +1,9 @@
+package kr.bb.store.exception.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ControllerAdvice {
+}
