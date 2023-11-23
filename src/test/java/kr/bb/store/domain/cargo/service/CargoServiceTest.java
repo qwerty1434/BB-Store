b69ce7a3153ca24692e338fceffa93dd775348e8
qwerty1434 <qwerty1434@naver.com>
2023-11-23 19:41:41 +0900

    :white_check_mark: 꽃 재고 추가 로직 테스트 #LF1-305

--- src/test/java/kr/bb/store/domain/cargo/service/CargoServiceTest.java
+++ src/test/java/kr/bb/store/domain/cargo/service/CargoServiceTest.java
@@ -35,12 +35,18 @@ class CargoServiceTest {
 
     @DisplayName("꽃 아이디와 수량을 입력받아 재고를 변경한다")
     @Test
-    void modifyStock() {
+    void modifyAllStocks() {
+        // given
         Store store = createStore();
         storeRepository.save(store);
-        FlowerCargo fc1 = createFlowerCargo(store.getId(), 2L, 100L, store);
-        FlowerCargo fc2 = createFlowerCargo(store.getId(), 1L, 100L, store);
-        FlowerCargo fc3 = createFlowerCargo(store.getId(), 3L, 100L, store);
+
+        FlowerCargoId flowerCargoId1 = createFlowerCargoId(store.getId(),2L);
+        FlowerCargoId flowerCargoId2 = createFlowerCargoId(store.getId(),1L);
+        FlowerCargoId flowerCargoId3 = createFlowerCargoId(store.getId(),3L);
+
+        FlowerCargo fc1 = createFlowerCargo(flowerCargoId1, 100L, store);
+        FlowerCargo fc2 = createFlowerCargo(flowerCargoId2, 100L, store);
+        FlowerCargo fc3 = createFlowerCargo(flowerCargoId3, 100L, store);
 
         flowerCargoRepository.saveAll(List.of(fc1,fc2,fc3));
 
@@ -48,12 +54,15 @@ class CargoServiceTest {
         StockModifyDto s2 = createStockModifyDto(2L, 6L);
         StockModifyDto s3 = createStockModifyDto(3L, 2L);
 
-        cargoService.modifyStock(store.getId(), List.of(s1,s2,s3));
+        // when
+        cargoService.modifyAllStocks(store.getId(), List.of(s1,s2,s3));
 
         em.flush();
         em.clear();
 
         List<FlowerCargo> flowerStocks = flowerCargoRepository.findAllByStoreId(store.getId());
+
+        // then
         assertThat(flowerStocks).hasSize(3)
                 .extracting("stock")
                 .containsExactlyInAnyOrder(
@@ -61,9 +70,53 @@ class CargoServiceTest {
                 );
     }
 
-    private FlowerCargo createFlowerCargo(Long storeId, Long flowerId, Long stock, Store store) {
+    @DisplayName("특정 가게, 특정 꽃의 재고를 수정한다")
+    @Test
+    void modifyStock() {
+        // given
+        Store store = createStore();
+        storeRepository.save(store);
+        FlowerCargoId flowerCargoId = createFlowerCargoId(store.getId(),1L);
+        FlowerCargo flowerCargo = createFlowerCargo(flowerCargoId, 100L, store);
+        flowerCargoRepository.save(flowerCargo);
+
+        // when
+        cargoService.PlusStockCount(store.getId(), 1L, 10L);
+
+        em.flush();
+        em.clear();
+
+        FlowerCargo flowerCargoFromDB = flowerCargoRepository.findById(flowerCargoId).get();
+        // then
+        assertThat(flowerCargoFromDB.getStock()).isEqualTo(110L);
+
+    }
+    
+    @DisplayName("재고는 음수가 될 수 없다")
+    @Test
+    void stockCannotBeNegative() {
+        // given
+        Store store = createStore();
+        storeRepository.save(store);
+        FlowerCargoId flowerCargoId = createFlowerCargoId(store.getId(),1L);
+        FlowerCargo flowerCargo = createFlowerCargo(flowerCargoId, 100L, store);
+        flowerCargoRepository.save(flowerCargo);
+
+        // when
+        cargoService.PlusStockCount(store.getId(), 1L, -10000L);
+
+        em.flush();
+        em.clear();
+
+        FlowerCargo flowerCargoFromDB = flowerCargoRepository.findById(flowerCargoId).get();
+        // then
+        assertThat(flowerCargoFromDB.getStock()).isEqualTo(0L);
+
+    }
+
+    private FlowerCargo createFlowerCargo(FlowerCargoId flowerCargoId, Long stock, Store store) {
         return FlowerCargo.builder()
-                .id(createFlowerCargoId(storeId,flowerId))
+                .id(flowerCargoId)
                 .store(store)
                 .stock(stock)
                 .build();
