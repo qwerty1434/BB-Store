7a66159eb19fade62fb3c58752a44f7d5754abac
qwerty1434 <qwerty1434@naver.com>
2023-11-28 17:20:19 +0900

    :recycle: 패키지 경로 설정

--- src/main/java/kr/bb/store/domain/store/repository/StoreRepositoryCustomImpl.java
+++ src/main/java/kr/bb/store/domain/store/repository/StoreRepositoryCustomImpl.java
@@ -5,7 +5,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import kr.bb.store.domain.store.entity.Store;
 import kr.bb.store.domain.store.entity.address.Gugun;
 import kr.bb.store.domain.store.entity.address.Sido;
-import kr.bb.store.domain.store.handler.response.QStoreForMapResponse;
+import kr.bb.store.domain.store.controller.response.QStoreForMapResponse;
 import kr.bb.store.domain.store.controller.response.StoreForMapResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
