85261426fbbb611ea12dee5a00ba3f7eb1271cc3
qwerty1434 <qwerty1434@naver.com>
2023-12-26 21:04:20 +0900

    :sparkles: 공통 에러 처리

--- src/main/java/kr/bb/store/exception/advice/ControllerAdvice.java
+++ src/main/java/kr/bb/store/exception/advice/ControllerAdvice.java
@@ -1,5 +1,7 @@
 package kr.bb.store.exception.advice;
 
+import bloomingblooms.response.CommonResponse;
+import kr.bb.store.exception.CustomException;
 import kr.bb.store.exception.NonPropagatingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +14,12 @@ public class ControllerAdvice {
     @ExceptionHandler(NonPropagatingException.class)
     public void nonPropagatingException(NonPropagatingException e) {
         log.warn("this error [{}] will not be thrown to user. rest logic will work well", e.getMessage());
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public CommonResponse customException(CustomException e) {
+        log.error(e.getMessage());
+        return CommonResponse.fail(e.getMessage(), "CE-01");
     }
 
 }
