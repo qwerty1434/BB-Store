0faa9ce2efc58034ee8d35dc135e349a6295eca3
qwerty1434 <qwerty1434@naver.com>
2023-12-26 20:59:32 +0900

    :recycle: answer create코드 변경에 따른 테스트코드 수정

--- src/main/java/kr/bb/store/exception/advice/ControllerAdvice.java
+++ src/main/java/kr/bb/store/exception/advice/ControllerAdvice.java
@@ -5,8 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
 @Slf4j
+@RestControllerAdvice
 public class ControllerAdvice {
 
     @ExceptionHandler(NonPropagatingException.class)
