e3d0f0aecae73e8aef0a6fc7b18d081cfc69ae0c
qwerty1434 <qwerty1434@naver.com>
2024-01-16 17:02:47 +0900

    :pencil2: 유저의 쿠폰 조회시 불필요한 Join으로 이상한 값이 보이던 문제 해결

--- src/main/java/kr/bb/store/domain/coupon/repository/CouponRepositoryCustomImpl.java
+++ src/main/java/kr/bb/store/domain/coupon/repository/CouponRepositoryCustomImpl.java
@@ -39,8 +39,6 @@ public class CouponRepositoryCustomImpl implements CouponRepositoryCustom{
                         coupon.endDate
                 ))
                 .from(coupon)
-                .leftJoin(issuedCoupon)
-                .on(coupon.id.eq(issuedCoupon.id.couponId))
                 .where(
                         coupon.store.id.eq(storeId),
                         coupon.isDeleted.isFalse(),
