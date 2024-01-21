1c7f09ae124668ed8bae15045d045633ba3e0153
qwerty1434 <qwerty1434@naver.com>
2024-01-21 16:00:09 +0900

    fix : 서킷 설정 변경

--- src/main/java/kr/bb/store/config/Resilience4jConfig.java
+++ src/main/java/kr/bb/store/config/Resilience4jConfig.java
@@ -15,11 +15,11 @@ public class Resilience4jConfig {
                 .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
                 .slidingWindowSize(10) // 슬라이딩 윈도우 크기
                 .failureRateThreshold(20) // 서킷을 open할 실패 비율
-                .slowCallDurationThreshold(Duration.ofSeconds(2)) // 느린응답 조건
+                .slowCallDurationThreshold(Duration.ofSeconds(5)) // 느린응답 조건
                 .slowCallRateThreshold(20) // 서킷을 open할 느린응답 비율
                 .minimumNumberOfCalls(5) // 서킷 확인을 위한 최소 요청 개수
                 .waitDurationInOpenState(Duration.ofMinutes(5)) // open에서 half-open이 되기까지의 시간
-                .maxWaitDurationInHalfOpenState(Duration.ofMinutes(5)) // half-open상태의 최대유지기간
+                .maxWaitDurationInHalfOpenState(Duration.ofMinutes(1)) // half-open상태의 최대유지기간
                 .permittedNumberOfCallsInHalfOpenState(3) // half-open상태에서 받아들일 요청 개수
                 .build();
     }
